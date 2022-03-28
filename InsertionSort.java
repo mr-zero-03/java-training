c81ec819320a71a5bad5ca699159d51d94aff043
mr-zero-03 <danielsantiagoperez03@gmail.com>
2022-03-28 13:57:46 -0500

    The Insertion Sort can also order from greater to lower

--- InsertionSort.java
+++ InsertionSort.java
@@ -3,16 +3,23 @@ import java.util.Arrays;
 
 public class InsertionSort {
 
-  public static float[] sortArray( float array[] ) {
+  public static float[] sortArray( float array[], int order ) {
 
     int countArray = array.length;
     for( int i = 1; i < countArray; i++ ) {
       float temp = array[ i ];
       int j = i - 1;
 
-      while( ( j >= 0 ) && ( array[ j ] > temp ) ) {
-        array[ j + 1 ] = array[ j ];
-        j--;
+      if ( order == 1 ) {  //*FROM THE LOWEST TO THE HIGHEST
+        while( ( j >= 0 ) && ( array[ j ] > temp ) ) {
+          array[ j + 1 ] = array[ j ];
+          j--;
+        }
+      } else if ( order == 2 ) {  //*FROM THE HIGHEST TO THE LOWEST
+        while( ( j >= 0 ) && ( array[ j ] < temp ) ) {
+          array[ j + 1 ] = array[ j ];
+          j--;
+        }
       }
       array[ j + 1 ] = temp;
     }
@@ -22,7 +29,7 @@ public class InsertionSort {
 
   public static float[] receiveArrayValues() {
 
-    Scanner input = new Scanner(System.in);
+    Scanner input = new Scanner( System.in );
 
     //Array Size
     System.out.print( "Cantidad de números a ser ordenados: " );
@@ -50,9 +57,16 @@ public class InsertionSort {
     System.out.println( "\nEl array a ser ordenado es: " );
     System.out.println( Arrays.toString( array ) );
 
-    array = sortArray( array );
+    Scanner input = new Scanner( System.in );
+    System.out.println( "\n¿Cómo deseas organizarlo?" );
+    System.out.println( "  1. Menor a mayor" );
+    System.out.println( "  2. Mayor a menor" );
+    System.out.print( "Escoge tu opción: " );
+    int order = input.nextInt();
 
-    System.out.println( "El array ordenado es: " );
+    array = sortArray( array, order );
+
+    System.out.println( "\nEl array ordenado es: " );
     System.out.println( Arrays.toString( array ) );
 
   }
